5e3e2b079d73e0225c2090ef40ea173373a2ced5
Reginald Johnson <Reginald Johnson>
2017-06-04 23:55:34 -0400

    first commit

--- src/Components/ImageItem.js
+++ src/Components/ImageItem.js
@@ -3,7 +3,13 @@ import React from 'react';
 const ImageItem = (props) => {
     return(
         <article className="thumb">
-            <a href={props.src} className="image"><img src={props.src} alt="" /></a>
+            <a 
+            className="image"
+            style={{
+                backgroundImage: `url(${props.src})`,
+                cursor: 'pointer',
+                outline: '0px'
+            }}><img src={props.src} alt="" /></a>
             <h2>{props.source_url}</h2>
             <p></p>
         </article>
